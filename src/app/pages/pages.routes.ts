3b3ddac63ec81030cc721e87354f67e526fcd5cc
vaionix <salvador@vaionix.mx>
2018-03-29 10:34:18 -0600

    Modificaciones en los modulos

---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,19 @@
+import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+const pagesRutes: Routes = [
+    { path : '',
+        component : PagesComponent,
+        children : [
+            { path : 'dashboard', component : DashboardComponent },
+            { path : 'progress', component : ProgressComponent },
+            { path : 'graficias1', component : Graficas1Component },
+            { path : '', redirectTo : '/dashboard', pathMatch : 'full' }
+        ]
+    }
+];
+
+export const PAGES_ROUTES = RouterModule.forChild(  pagesRutes );
